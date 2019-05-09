f88b4906761e6d6359f327312361df5c108ae316
MladenPetrov88 <50301670+MladenPetrov88@users.noreply.github.com>
2019-05-09 17:06:42 +0300

    Add files via upload

---
+++ PetShop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PetShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dogs = Integer.parseInt(scanner.nextLine());
+        int otherAnimals = Integer.parseInt(scanner.nextLine());
+        double dogFood = dogs * 2.50;
+        double otherFood = otherAnimals * 4;
+        double totalFood = dogFood + otherFood;
+        System.out.printf("%.2f lv." , totalFood);
+    }
+}
+
